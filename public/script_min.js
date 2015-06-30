01b2d78d6b0b39d5cdc697cb739ea94ec279fcb0
John <midnighttripmusc@gmail.com>
2015-06-29 22:19:21 -0400

    send to pi

--- public/script_min.js
+++ public/script_min.js
@@ -1,10 +1,14 @@
 var mjpeg_img;
  
 function reload_img () {
-  mjpeg_img.src = "cam_pic.php?time=" + new Date().getTime();
+  mjpeg_img.src = "cam.jpg?time=" + new Date().getTime();
+
+  setTimeout(function() {
+	reload_img
+	}, 10);
 }
 function error_img () {
-  setTimeout("mjpeg_img.src = 'cam_pic.php?time=' + new Date().getTime();", 100);
+  setTimeout("mjpeg_img.src = 'cam.jpg?time=' + new Date().getTime();", 100);
 }
 function init() {
   mjpeg_img = document.getElementById("mjpeg_dest");
@@ -12,3 +16,4 @@ function init() {
   mjpeg_img.onerror = error_img;
   reload_img();
 }
+
